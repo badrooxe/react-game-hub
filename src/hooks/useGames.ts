7002a8557f0779441466e78e58d4df10ebfe198e
badrooxe <badrooxe55@gmail.com>
2023-10-19 15:00:26 +0100

    create a custom hook for fetching data / restructure GameGrid component

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "../services/api-client";
+
+interface Results {
+    id: number;
+    name: string;
+  }
+  
+  interface Games {
+    count: number;
+    results: Results[];
+  }
+
+const useGames = () => {
+  const [games, setGames] = useState<Results[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log("useEffect");
+    const controller = new AbortController();
+    setLoading(true);
+    //axios
+    apiClient
+      .get<Games>("/games", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, loading, error };
+
+};
+
+
+export default useGames;
