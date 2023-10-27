da0c867f2dfdbad3b66ad86caaab93b63c6efdbc
badrooxe <badrooxe55@gmail.com>
2023-10-27 17:33:01 +0100

    remove duplications

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react'
-import apiClient, { CanceledError } from '../services/api-client';
-
 export interface GenresResults {  
     id: number;
     name: string;
@@ -13,34 +10,3 @@ export interface Genres {
     count: number;
     results: GenresResults[];
 }
-const useGenres = () => {
-  const [genres, setGenres] = useState<GenresResults[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    //axios
-    apiClient
-      .get<Genres>("/genres", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  },[]);
-  
-  return {genres, loading, error};
-}
-
-export default useGenres;
